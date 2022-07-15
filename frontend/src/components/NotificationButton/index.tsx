b4b6f20e6d10c9af83b40f99d4486a7991a353ae
Mario Cesar de Souza jr <mcsjuniorsouza@gmail.com>
2022-07-15 11:05:51 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg';
 import { BASE_URL } from '../../utils/request';
 import './styles.css';
@@ -12,7 +13,10 @@ function NotificationButton({saleId}: Props) {
     function handleClick(saleId: number): void {
         axios.get(`${BASE_URL}/sales/${saleId}/notification`)
         .then((response) => {
-          console.log("Sucesso")
+          toast.info("SMS enviado com sucesso");
+        })
+        .catch(() => {
+            toast.error("Por favor, tente mais tarde");
         })
     }
 
